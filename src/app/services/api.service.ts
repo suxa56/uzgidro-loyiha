f15a25b5c7e2382247275f2112ead6decea0adb8
suxa56 <62641532+suxa56@users.noreply.github.com>
2023-10-17 12:22:19 +0500

    save token

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -7,11 +7,16 @@ import {HttpClient} from '@angular/common/http';
 export class ApiService {
   private siteUrl: string = 'https://8464-94-158-60-58.ngrok-free.app/'
   private loginUrl: string = 'api/login/'
+  private logoutUrl: string = 'api/logout/'
 
   constructor(private http: HttpClient) {
   }
 
   loginByAuth({email, password}) {
-    return  this.http.post(this.siteUrl + this.loginUrl, {username: email, password: password})
+    return this.http.post(this.siteUrl + this.loginUrl, {username: email, password: password})
+  }
+
+  logout() {
+    return this.http.get(this.siteUrl + this.logoutUrl)
   }
 }
--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -2,55 +2,124 @@ import {Injectable} from '@angular/core';
 import {Router} from '@angular/router';
 import {ToastrService} from 'ngx-toastr';
 import {Gatekeeper} from 'gatekeeper-client-sdk';
-import {HttpClient} from "@angular/common/http";
 import {ApiService} from "@services/api.service";
+import {CookieOptions, CookieService} from "ngx-cookie-service";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class AppService {
-    public user: any = null;
+  public user: any = null;
 
-    constructor(private router: Router, private toastr: ToastrService, private api: ApiService) {}
+  constructor(private router: Router, private toastr: ToastrService, private api: ApiService, private cookie: CookieService) {
+  }
 
-    loginByAuth({email, password}) {
-        try {
-          this.api.loginByAuth({email, password}).subscribe((response) => console.log(response))
-            // const token = await Gatekeeper.loginByAuth(email, password);
-            // localStorage.setItem('token', token);
-            // await this.getProfile();
-            // this.router.navigate(['/']);
-            // this.toastr.success('Login success');
-        } catch (error) {
-            this.toastr.error(error.message);
-        }
+  loginByAuth({email, password}) {
+    try {
+      this.api.loginByAuth({email, password}).subscribe({
+        next: (response: {token: string}) => {
+          if (response.token) {
+                  this.setToken(response.token)
+                  this.toastr.success('Login success');
+                }
+        },
+        error: error  => {
+            if (error.status === 409) {
+              this.logout()
+              this.loginByAuth({email, password})
+            } else {
+              this.toastr.error(error, 'Ошибка при запросе данных')
+            }
+        },
+        complete: () => {}
+      })
+      // const token = await Gatekeeper.loginByAuth(email, password);
+      // localStorage.setItem('token', token);
+      // await this.getProfile();
+      // this.router.navigate(['/']);
+      // this.toastr.success('Login success');
+    } catch (error) {
+      this.toastr.error(error.message);
     }
+  }
 
-    async registerByAuth({email, password}) {
-        try {
-            const token = await Gatekeeper.registerByAuth(email, password);
-            localStorage.setItem('token', token);
-            await this.getProfile();
-            this.router.navigate(['/']);
-            this.toastr.success('Register success');
-        } catch (error) {
-            this.toastr.error(error.message);
-        }
+  async registerByAuth({email, password}) {
+    try {
+      const token = await Gatekeeper.registerByAuth(email, password);
+      localStorage.setItem('token', token);
+      await this.getProfile();
+      this.router.navigate(['/']);
+      this.toastr.success('Register success');
+    } catch (error) {
+      this.toastr.error(error.message);
     }
+  }
 
-    async getProfile() {
-        try {
-            this.user = await Gatekeeper.getProfile();
-        } catch (error) {
-            this.logout();
-            throw error;
-        }
+  async getProfile() {
+    try {
+      this.user = await Gatekeeper.getProfile();
+    } catch (error) {
+      this.logout();
+      throw error;
     }
+  }
 
-    logout() {
-        localStorage.removeItem('token');
-        localStorage.removeItem('gatekeeper_token');
-        this.user = null;
-        this.router.navigate(['/login']);
+  logout() {
+    this.api.logout()
+    this.setToken(null)
+    this.user = null;
+    this.router.navigate(['/login']);
+  }
+
+  private setToken(response: string | null) {
+    if (response) {
+      const options: CookieOptions = {
+        path: '/',
+        sameSite: "Strict",
+        secure: true,
+        domain: undefined,
+        expires: new Date(new Date().getTime() + 3600 * 1000)
+      }
+      this.cookie.set('jwt', response, options)
+    } else {
+      this.cookie.deleteAll()
     }
+  }
 }
+
+
+// message
+//   :
+//   "Muvaffaqiyatli kirish"
+// token
+//   :
+//   "5ff568889caaa525dde1d1667f012f2230a02b51"
+// user
+//   :
+//   id
+//     :
+//     1
+// is_builder
+//   :
+//   false
+// is_chief_director
+//   :
+//   false
+// is_chief_supervisor
+//   :
+//   false
+// is_designer
+//   :
+//   false
+// is_director
+//   :
+//   false
+// is_supervisor
+//   :
+//   false
+// is_tex_supervisor
+//   :
+//   false
+// username
+//   :
+//   "admin"

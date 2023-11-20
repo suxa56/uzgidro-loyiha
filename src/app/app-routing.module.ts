5d7192b8ad801b97a8663057820272cbf3e67b12
suxa56 <62641532+suxa56@users.noreply.github.com>
2023-11-20 12:19:17 +0500

    remove auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,8 +17,8 @@ const routes: Routes = [
     {
         path: '',
         component: MainComponent,
-        canActivate: [AuthGuard],
-        canActivateChild: [AuthGuard],
+        // canActivate: [AuthGuard],
+        // canActivateChild: [AuthGuard],
         children: [
             {
                 path: 'profile',
